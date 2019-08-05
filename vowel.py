0ec31839cb2ca5a8c800275e94bfc9a4ff6d3666
pavithradheep <52281064+pavithradheep@users.noreply.github.com>
2019-08-05 14:24:44 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,7 @@
+c=input()
+if c=='a' or c=='e'or c=='i' or c=='o' or c=='u':
+	print("Vowel")
+elif c=='b' or c=='c'or c=='d' or c=='f'or c=='g'or c=='h'or c=='j' or c=='k'or c=='l'or c=='m'or c=='n'or c=='p'or c=='q'or c=='r'or c=='s'or c=='t'or c=='v'or c=='w'or c=='x' or c=='y' or c=='z':
+	print("Consonant")
+else:
+	print("invalid")
