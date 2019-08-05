87315ea38b72d3f2a7368d45c558ce3429719f81
pavithradheep <52281064+pavithradheep@users.noreply.github.com>
2019-08-05 14:42:44 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,5 @@
+c=input()
+if c=='a' or c=='b' or c=='c'or c=='d'or c=='e' or c=='f' or c=='g' or c=='h' or c=='i' or c=='j' or c=='k' or c=='l' or c=='m' or c=='n' or c=='o' or c=='p' or c=='q' or c=='r' or c=='s' or c=='t' or c=='u' or c=='v' or c=='w' or c=='x' or c=='y' or c=='z':
+	print("Alphabet")
+else:
+	print("No")
